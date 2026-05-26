259fb2a51278e15b198ff30c0f7b6116caebd09d
Madhumitha S <madhuaug48@gmail.com>
2026-05-26 22:19:31 +0530

    Update sw.js

--- src/main/resources/static/sw.js
+++ src/main/resources/static/sw.js
@@ -1,3 +1,7 @@
-self.addEventListener("install", () => {
-  console.log("SW installed");
+self.addEventListener("install", (event) => {
+    console.log("Service Worker Installed");
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(fetch(event.request));
 });
