df33e8e1aabf9b6def961d2bf3d18921dfd5feeb
Madhumitha S <madhuaug48@gmail.com>
2026-05-27 11:15:14 +0530

    Update ContactService.java

--- src/main/java/com/example/Portfolio1/service/ContactService.java
+++ src/main/java/com/example/Portfolio1/service/ContactService.java
@@ -4,6 +4,7 @@ import com.example.Portfolio1.entity.Contact;
 import com.example.Portfolio1.repository.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 public class ContactService {
@@ -14,4 +15,8 @@ public class ContactService {
     public Contact save(Contact contact) {
         return repository.save(contact);
     }
+
+public List<Contact> getAll() {
+    return repository.findAll();
+}
 }
